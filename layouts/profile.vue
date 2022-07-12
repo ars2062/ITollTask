bdf94974a663d877cefa89fde817bea4ffeaa476
Arshia Moghaddam <ars2062@gmail.com>
2022-07-12 18:07:27 +0430

    feat: profile edit

---
+++ layouts/profile.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" data-app>
+    <login-dialog />
+    <app-header />
+    <main>
+      <Nuxt />
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import AppHeader from '@/components/layout/AppHeader/index.vue'
+import LoginDialog from '~/components/Auth/AuthDialog.vue'
+export default Vue.extend({
+  components: { AppHeader, LoginDialog },
+  watch: {
+    '$auth.loggedIn': {
+      handler(val) {
+        if (!val) this.$router.push('/')
+      },
+      immediate: true,
+    },
+  },
+})
+</script>
